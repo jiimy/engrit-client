68d85524863ec3ee9cfd34149b3b7dc2da4a25f9
rudwnok <rudwnok123@naver.com>
2024-09-24 20:41:46 +0900

    빌드 에러 수정 및 배포 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 'use client';
 import BottomMenu from "@/components/bottomMenu/BottomMenu";
+import Header from "@/components/header/Header";
 import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <div className='content'>
         메인페이지
-        <br />배포 테스트
+
       </div>
       <BottomMenu />
     </div>
