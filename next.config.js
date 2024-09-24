5c43640c26eedd5c25bdbbbd121a77e76f4a1849
rudwnok <rudwnok123@naver.com>
2024-09-24 19:29:22 +0900

    out 테스트

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
 const nextConfig = {
-  output: "export",
-  distDir: "out",
+  // output: "export",
+  // distDir: "out",
   sassOptions: {
     includePaths: [path.join(__dirname, "src/assets/styles")],
     prependData: `@import "_mixins.scss"; @import "_variables";`,
