077f5107046b979e49dd895e8b4013f88dcda076
rudwnok <rudwnok123@naver.com>
2024-10-10 01:08:39 +0900

    업로드, 수정 공통페이지

---
+++ src/app/(editPage)/(shared)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SharedPage = () => {
+  return (
+    <div>
+      공통페이지
+    </div>
+  );
+};
+
+export default SharedPage;
---
+++ src/app/(editPage)/upload/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SharedPage from '../(shared)/page';
+
+const page = () => {
+  return (
+    <div>
+      <SharedPage />
+    </div>
+  );
+};
+
+export default page;
