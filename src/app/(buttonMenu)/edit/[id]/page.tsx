5fe5fdca56047dcc91d1777744997eff8e1b1bf3
rudwnok <rudwnok123@naver.com>
2024-10-25 08:56:41 +0900

    ignore 수정

---
+++ src/app/(buttonMenu)/edit/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Edit = () => {
+  return (
+    <div>
+      수정페이지
+    </div>
+  );
+};
+
+export default Edit;
--- src/components/portalModal/shareModal/ShareModal.tsx
+++ src/components/portalModal/shareModal/ShareModal.tsx
@@ -29,12 +29,11 @@ const ShareModal = ({
   }, []);
 
   const shareMessage = () => {
-    console.log('클릭');
     Kakao?.Share.sendDefault({
       objectType: 'feed',
       content: {
-        title: '딸기 치즈 케익',
-        description: '#케익 #딸기 #삼평동 #카페 #분위기 #소개팅',
+        title: '유튜브 제목',
+        description: '유튜브 태그들',
         imageUrl:
           'http://k.kakaocdn.net/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
         link: {
@@ -43,27 +42,27 @@ const ShareModal = ({
           webUrl: 'https://developers.kakao.com',
         },
       },
-      social: {
-        likeCount: 286,
-        commentCount: 45,
-        sharedCount: 845,
-      },
-      buttons: [
-        {
-          title: '웹으로 보기',
-          link: {
-            mobileWebUrl: 'https://developers.kakao.com',
-            webUrl: 'https://developers.kakao.com',
-          },
-        },
-        {
-          title: '앱으로 보기',
-          link: {
-            mobileWebUrl: 'https://developers.kakao.com',
-            webUrl: 'https://developers.kakao.com',
-          },
-        },
-      ],
+      // social: {
+      //   likeCount: 286,
+      //   commentCount: 45,
+      //   sharedCount: 845,
+      // },
+      // buttons: [
+      //   {
+      //     title: '웹으로 보기',
+      //     link: {
+      //       mobileWebUrl: 'https://developers.kakao.com',
+      //       webUrl: 'https://developers.kakao.com',
+      //     },
+      //   },
+      //   {
+      //     title: '앱으로 보기',
+      //     link: {
+      //       mobileWebUrl: 'https://developers.kakao.com',
+      //       webUrl: 'https://developers.kakao.com',
+      //     },
+      //   },
+      // ],
     });
   }
 
