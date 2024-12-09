7c4945f0201ef3465fe5d2a3daafeb1eb3361a11
rudwnok <rudwnok123@naver.com>
2024-12-09 12:51:35 +0900

    수정

--- src/app/(bottomMenu)/support/[id]/page.tsx
+++ src/app/(bottomMenu)/support/[id]/page.tsx
@@ -1,29 +1,28 @@
 'use client';
 
 import { getInquiriesID } from '@/api/inquiries';
+import { useLayoutContext } from '@/context/LayoutContext';
 import { dayformatTime } from '@/util/day';
 import { useQuery } from '@tanstack/react-query';
-import { useParams, useRouter } from 'next/navigation';
+import { useParams } from 'next/navigation';
 import s from '../support.module.scss';
-import { useContext, useEffect } from 'react';
-import { TitleContext } from '../layout';
 
 const SupportDetail = () => {
-  const { setTitle } = useContext(TitleContext);
+  const { setText } = useLayoutContext();
   const params = useParams<{ id: string }>();
   const inquiryId = Number(params?.id);
-  
+
   const { data, isLoading, error, isSuccess } = useQuery({
     queryKey: ["getInquiriesDetail", inquiryId],
     queryFn: () => getInquiriesID(inquiryId),
     // enabled: !!inquiryId,
   });
 
-  if(isSuccess) {
+  if (isSuccess) {
     if (data[0]?.response_text) {
-      setTitle('문의내용')
+      setText('문의내용')
     } else {
-      setTitle('문의하기')
+      setText('문의하기')
     }
 
   }
