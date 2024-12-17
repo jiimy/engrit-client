258eb1d8fe2f32cc54187fec939dfa6c7af6056d
rudwnok <rudwnok123@naver.com>
2024-12-17 22:35:13 +0900

    내가 올린 영상 데이터 read

--- src/app/(bottomMenu)/mypage/upload/page.tsx
+++ src/app/(bottomMenu)/mypage/upload/page.tsx
@@ -1,24 +1,39 @@
 'use client';
-import PieChart from '@/components/chart/PieChart';
-import { isLogin } from '@/util/authCookie';
-import { redirect } from 'next/navigation';
-import React, { useEffect } from 'react';
+import { myYoutubeUplaodApi } from '@/api/youtube';
+import { getRelativeTimeDay } from '@/util/day';
+import { useQuery } from '@tanstack/react-query';
 
 const MyPageUpload = () => {
-  
+
+  const { data } = useQuery({
+    queryFn: () => myYoutubeUplaodApi(),
+    queryKey: ['myYoutubeUpload'],
+    staleTime: 1000 * 60 * 5
+  });
+
+  // const sortDay = data?.sort((a: any, b: any) => a.created_at - b.created_at);
+  const sortDay = data?.sort(
+    (a: any, b: any) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+  );
+  console.log('dupl', data, sortDay);
+
   return (
     <>
       <ul>
         <li>
-          <div>
-            영상
-          </div>
-          <div>
-            <div className="title">제목 1줄
-              <span>더보기아이콘</span>
+          {sortDay?.map((item: any, index: any) => (
+            <div key={index}>
+              <div>
+                {item.youtube_link}
+              </div>
+              <div>
+                <div className="title">제목 1줄
+                  <span>더보기아이콘</span>
+                </div>
+                {getRelativeTimeDay(item.created_at)}일 전
+              </div>
             </div>
-            n일전
-          </div>
+          ))}
         </li>
       </ul>
     </>
