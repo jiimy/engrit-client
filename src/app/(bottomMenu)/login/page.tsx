392d50f01c9a0120c1424904ca153d7c87114031
rudwnok <rudwnok123@naver.com>
2025-01-03 08:21:34 +0900

    env 적용으로 수정

--- src/app/(bottomMenu)/login/page.tsx
+++ src/app/(bottomMenu)/login/page.tsx
@@ -16,12 +16,12 @@ const Index = () => {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: 'kakao',
       options: {
-        // redirectTo: process.env.NEXT_PUBLIC_VERCEL_URL
-        //   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/auth/callback`
-        //   : 'http://localhost:3000/auth/callback'
+        redirectTo: process.env.NEXT_PUBLIC_VERCEL_URL
+          ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/auth/callback`
+          : 'http://localhost:3000/auth/callback'
         // redirectTo: `${window.location.origin}/auth/callback`
         // redirectTo: location.origin + "/auth/callback",
-        redirectTo: 'http://localhost:3000/auth/callback'
+        // redirectTo: 'http://localhost:3000/auth/callback'
       },
     });
 
