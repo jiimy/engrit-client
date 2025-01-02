b3482353cc7c3dd21414205591febaae1577a8f9
rudwnok <rudwnok123@naver.com>
2025-01-02 22:41:51 +0900

    로그인 테스트중..

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -2,20 +2,22 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import supabase from "@/util/supabase/client";
+import { createClient } from "@/util/supabase/client";
 
 export default function AuthCallback() {
+  const supabase = createClient();
   const router = useRouter();
 
   useEffect(() => {
     const checkSession = async () => {
       const { data, error } = await supabase.auth.getSession();
-      if (error || !data.session) {
-        console.error("Authentication failed:", error);
-        router.push("/login");
-      } else {
-        router.push("/"); // 로그인 후 이동할 페이지
-      }
+      console.log('data', data);
+      // if (error || !data.session) {
+      //   console.error("Authentication failed:", error);
+      //   router.push("/login");
+      // } else {
+      //   router.push("/"); // 로그인 후 이동할 페이지
+      // }
     };
 
     checkSession();
